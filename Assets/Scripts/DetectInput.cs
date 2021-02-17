6931a43b0f24729d3e23488445fadbcc29bb56d4
Marie Duffy <meduffy@sdsu.edu>
2021-02-17 14:23:45 -0800

    Initial commit

---
+++ Assets/Scripts/DetectInput.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DetectInput : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var input = gameObject.GetComponent<InputField>();
+        var se= new InputField.SubmitEvent();
+        se.AddListener(displayText);
+        input.onEndEdit = se;
+
+    }
+
+    private void displayText(string textInField)
+    {
+        Debug.Log(textInField);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
